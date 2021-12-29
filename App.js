f944bf9eb6e84b082b6f9dc613b84cc539e2afe8
[ben-random] <[Benjamin.girvan@icloud.com]>
2021-12-29 15:25:22 +0000

    Added safe area view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   console.log("App executed");
@@ -15,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
