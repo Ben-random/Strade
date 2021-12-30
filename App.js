ca5e8ebc200b4cce0079f95972f6f9eb18b75257
[ben-random] <[Benjamin.girvan@icloud.com]>
2021-12-30 14:32:48 +0000

    Added alternate page, styles, page navigation etc

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, Image, TouchableHighlight, Alert, View, Button, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  console.log("App executed");
-  const handlePress = () => console.log("Text clicked");
+  console.log("Application started")
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Stocks" component={StocksScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+const HomeScreen = ({navigation}) => {
+  return(
     <SafeAreaView style={styles.container}>
       <TouchableHighlight onPress={() => console.log("Image Pressed")}>
       <Image source={{
@@ -12,16 +26,27 @@ export default function App() {
         height: 100,
         uri: "https://www.pngkit.com/png/detail/124-1246922_stripe-logo-stripe-png-logo.png"}}/>
       </TouchableHighlight>
-      <Text numberOfLines={1} onPress={handlePress}>
+      <Text numberOfLines={1}>
         Welcome to Strade -  the risk free stcok market simulator
       </Text>
-      <Button title='Log in' onPress={() => Alert.prompt("Log in", "Username", (text) => console.log("User: " + text))}
+      <Button title='Log in' onPress={() => Alert.prompt("Log in", "Username", (text) => navigation.navigate("Stocks"))}
       />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
-}
-
+};
+const StocksScreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={AltStyle.container}>
+      <text style={[{fontSize: 45}]}>
+        Stocks
+      </text>
+        <TouchableHighlight>
+        <SafeAreaView/>
+        </TouchableHighlight>
+    </SafeAreaView>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -29,4 +54,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
+});
+
+const AltStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: "center"
+  }
 });
