9025e55109b1267a99d16b74849ddc4ba65ddfc8
[ben-random] <[Benjamin.girvan@icloud.com]>
2022-01-06 12:01:59 +0000

    Fixing Ajax issue

--- StockPricesScheduler.js
+++ StockPricesScheduler.js
@@ -1,20 +1,20 @@
-const GetStockPrices = () => {
-    $.ajax({
-        url: "https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=FB%2C%20AMZN%2C%20GOOG%2C%20NFLX",
-        beforeSend: function(xhr) {
-           xhr.setRequestHeader("'X-API-KEY", "dLcdplO5qt8urK9hfdFce4RIHd7qNqqi2HFYRIIa")
-        },
-        success: function(data) {
-           alert(data);
-           const Prices = []
-           result = { data }.result
-           for (let i = 0; i < 4; i++) {
+import { $ } from "jquery"
+
+$.ajax({
+    url: "https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=FB%2C%20AMZN%2C%20GOOG%2C%20NFLX",
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader("'X-API-KEY", "dLcdplO5qt8urK9hfdFce4RIHd7qNqqi2HFYRIIa")
+    },
+    success: function(data) {
+        alert(data);
+        const Prices = []
+        result = { data }.result
+        for (let i = 0; i < 4; i++) {
             Price = result[i].bid
             Prices.push(Price)
-           }
         }
-     })
-    const url = "https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=FB%2C%20AMZN%2C%20GOOG%2C%20NFLX"
-    return Prices
-}
-return GetStockPrices()
+        return Prices
+    }
+})
+const StockPrices = Prices
+export { StockPrices }
