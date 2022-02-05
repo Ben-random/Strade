628244c3c529e92f3c86bab4e715bcf824428b4b
[ben-random] <[Benjamin.girvan@icloud.com]>
2022-02-05 13:55:07 +0000

    Fixing stock data scraping

--- StockPricesScheduler.js
+++ StockPricesScheduler.js
@@ -6,7 +6,7 @@ const Stocks = ["FB", "AMZN", "GOOG", "NFLX"]
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = "c3slhlqad3ide69e45dg"
 const finnhubClient = new finnhub.DefaultApi()
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < Stocks.length; i++) {
     const Stock = Stocks[i];
     finnhubClient.quote(Stock, (error, data, response) => {
         Prices.push(data.c)
