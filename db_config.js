05cf495394e12ea8d389bd302fa72c5f2ac342b2
[ben-random] <[Benjamin.girvan@icloud.com]>
2022-04-08 11:53:18 +0100

    Config of Mongo db

---
+++ db_config.js
@@ -0,0 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Datastore from 'react-native-local-mongodb';
+
+const db = new Datastore({
+  filename: 'asyncStorageKey',
+  storage: AsyncStorage
+});
+
+export { db }
+
+
